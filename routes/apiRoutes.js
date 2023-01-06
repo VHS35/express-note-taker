d8aa6707753783533778e2eceaf0ef9b7aafb117
VHS35 <110948699+VHS35@users.noreply.github.com>
2023-01-05 23:21:56 -0600

    repo update

---
+++ routes/apiRoutes.js
@@ -0,0 +1,68 @@
+
+const fs = require('fs');
+const uuid = require('uuid');//npm uuid gives notes unique id 
+const router = require('express').Router();
+require('../db/db.json')
+
+//mini project helper code
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helperCode/fsUtils');
+
+//set up router to read file and content to db.json file
+router.get('/', (req, res) => {
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('../db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+// POST Route for a new note
+router.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid.v4(),
+      };
+  
+      readAndAppend(newNote, '../db/db.json');
+      res.json(`Note created!`);
+    } else {
+      res.error('Error in creating note');
+    }
+});
+
+// DELETE Route for a specific note
+router.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('../db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Makes a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Saves that array to the filesystem
+        writeToFile('../db/db.json', result);
+  
+        // Response to the DELETE request
+        res.json("Note has been deleted");
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,24 +2,27 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 
-
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 
-
+//middleware
 app.use(express.static('public'));
-
+app.use('/api', apiRoutes);
+//middleware that access the data from the request body 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 //gets route for homepage index file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
-  //gets rout for notes page
+  //gets route for notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
 
-//we want the app to listen
+// code for the app to listen to server
   app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
